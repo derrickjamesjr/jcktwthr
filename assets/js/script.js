46bf4b1328ce54e2763fbe90b6b120b601bab646
Derrick James <ballyouth@gmail.com>
2022-06-12 09:48:05 -0400

    object,object appearing on current weather list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var lctn = $(".lctn");
 var weatherinfo = function(input) {    
     var city = input;
     console.log(city);
-    var apistr = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=3e6e91e6c8387cdac97df82b9bc87d27"
+    var apistr = "https://api.openweathermap.org/geo/1.0/direct?q=" + city +"&limit=5&appid=3e6e91e6c8387cdac97df82b9bc87d27"
     console.log(apistr);
     //Gets map coordinates from inputed city
     // fetch("https://api.openweathermap.org/data/2.5/weather?q=Irvington,us&APPID=3e6e91e6c8387cdac97df82b9bc87d27")
@@ -17,8 +17,8 @@ var weatherinfo = function(input) {
         // console.log(data.coord.lat, data.coord.lon);
         localStorage.setItem("coordlat", data[0].lat);
         localStorage.setItem("coordlon", data[0].lon);
-        console.log(data);
-        console.log(data[0].lat, data[0].lon);
+        // console.log(data);
+        // console.log(data[0].lat, data[0].lon);
       })};
 
      var dailyfrcst = function() {
@@ -27,14 +27,26 @@ var weatherinfo = function(input) {
         //get lat and lon from inputted city
         var apistr = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon +"&exclude=hourly&appid=3e6e91e6c8387cdac97df82b9bc87d27";
         // "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=94.04&exclude=hourly&appid=3e6e91e6c8387cdac97df82b9bc87d27"
+        var dsplywthr = $("#thiswthr");
+        
+
         fetch(apistr)
         .then(function(response) {
             return response.json();
           })
           .then(function(data) {
             // console.log(data.coord.lat, data.coord.lon);
-            for (var i = 0; i < 4; i++) {
-            console.log(data.daily[i]);
+            for (var i = 0; i < 5; i++) {
+            // console.log(data.daily[i]);
+            var wthrrpt = data.daily[i].weather;
+            // console.log(wthrrpt);
+            var cityLi = document.createElement("li");
+            cityLi.append(wthrrpt);
+            // console.log(cityLi);
+            // console.log(wthrrpt);
+            dsplywthr.append(cityLi);
+
+
           }})}
 
       dailyfrcst();
