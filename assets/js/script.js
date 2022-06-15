b372760ef92b66343332d313a12deb8e669fb728
Derrick James <ballyouth@gmail.com>
2022-06-15 19:50:41 -0400

    displays weather string

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 var userinput = $("#city")
 var btn = $(".btn");
 var lctn = $(".lctn");
+var dycntnr = $("#thiswthr");
 
 var weatherinfo = function(input) {    
     var city = input;
-    console.log(city);
+    // console.log(city);
     var apistr = "https://api.openweathermap.org/geo/1.0/direct?q=" + city +"&limit=5&appid=3e6e91e6c8387cdac97df82b9bc87d27"
-    console.log(apistr);
+    // console.log(apistr);
     //Gets map coordinates from inputed city
     // fetch("https://api.openweathermap.org/data/2.5/weather?q=Irvington,us&APPID=3e6e91e6c8387cdac97df82b9bc87d27")
     fetch(apistr)
@@ -19,6 +20,7 @@ var weatherinfo = function(input) {
         localStorage.setItem("coordlon", data[0].lon);
         // console.log(data);
         // console.log(data[0].lat, data[0].lon);
+        
       })};
 
      var dailyfrcst = function() {
@@ -36,15 +38,14 @@ var weatherinfo = function(input) {
           })
           .then(function(data) {
             // console.log(data.coord.lat, data.coord.lon);
+            
             for (var i = 0; i < 5; i++) {
+            
+            var wthrrpt = data.daily[i];
             // console.log(data.daily[i]);
-            var wthrrpt = data.daily[i].weather;
-            // console.log(wthrrpt);
-            var cityLi = document.createElement("li");
-            cityLi.append(wthrrpt);
-            // console.log(cityLi);
-            // console.log(wthrrpt);
-            dsplywthr.append(cityLi);
+            var daylg = document.createElement("div");
+            daylg.innerHTML = JSON.stringify(data.daily[i].weather);
+            dycntnr.append(daylg);
 
 
           }})}
